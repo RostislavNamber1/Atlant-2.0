c5895208123b88120bf5edeade9956672c59497e
”Rostislav” <rostislavdasenkr@gmail.com>
2022-12-20 01:20:12 +0500

    go

---
+++ js/js.js
@@ -0,0 +1,31 @@
+var swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      type: 'bullets',
+    },
+    mousewheel: true,
+    keyboard: true,
+    loop: true,
+  });
+
+
+  var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
